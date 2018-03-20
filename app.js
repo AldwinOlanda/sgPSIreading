534acd7cd5e7db2974de55d19627b49dff5263cd
AldwinOlanda <35451225+AldwinOlanda@users.noreply.github.com>
2018-03-20 14:40:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ function callPSIApi(datetime,date) {
                 //for (i = 0; i != psitwentyfourhourly.length; i++) {
                   
                     //if (psitwentyfourhourly[i]['area']==location){
-                  output = 'Here are the PSI reading in Singapore '; 
+                  output = 'Here are the PSI reading in Singapore ' +
                           //+
                           '  National : '+ psitwentyfourhourly[0]['national'] +' '
                           // '  North : '+ psitwentyfourhourly[0]['north'] +' '+
